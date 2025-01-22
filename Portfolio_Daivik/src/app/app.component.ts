39345c035ab595724a1cbeae3ad09b6c02536545
daivik1515 <daivik.15.saharan@gmail.com>
2025-01-21 19:40:25 -0500

    Added contact page

--- Portfolio_Daivik/src/app/app.component.ts
+++ Portfolio_Daivik/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavbarComponent } from './views/navbar/navbar.component';
 import { AboutComponent } from './views/about/about.component';
 import { SkillsComponent } from './views/skills/skills.component';
 import { SkillCardsComponent } from './views/skill-cards/skill-cards.component';
+import { ProjectsComponent } from './views/projects/projects.component';
+import { ContactComponent } from './views/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,AboutComponent,SkillsComponent,SkillCardsComponent],
+  imports: [RouterOutlet,NavbarComponent,AboutComponent,SkillsComponent,SkillCardsComponent,ProjectsComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
