ce5504a0d084e90ce691fe1dd1fa6c4640cc6c7c
daivik1515 <daivik.15.saharan@gmail.com>
2025-01-22 19:12:54 -0500

    Added Education

--- Portfolio_Daivik/src/app/app.component.ts
+++ Portfolio_Daivik/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { SkillsComponent } from './views/skills/skills.component';
 import { SkillCardsComponent } from './views/skill-cards/skill-cards.component';
 import { ProjectsComponent } from './views/projects/projects.component';
 import { ContactComponent } from './views/contact/contact.component';
+import { EducationComponent } from './views/education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,AboutComponent,SkillsComponent,SkillCardsComponent,ProjectsComponent,ContactComponent],
+  imports: [RouterOutlet,NavbarComponent,AboutComponent,SkillsComponent,SkillCardsComponent,ProjectsComponent,ContactComponent,EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
