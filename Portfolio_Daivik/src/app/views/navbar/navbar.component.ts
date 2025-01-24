c8df60e1e0de9cabc4b3ff7bbb28e6615dbd274e
daivik1515 <daivik.15.saharan@gmail.com>
2025-01-24 04:00:41 -0500

    Did UI changes

--- Portfolio_Daivik/src/app/views/navbar/navbar.component.ts
+++ Portfolio_Daivik/src/app/views/navbar/navbar.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
+
