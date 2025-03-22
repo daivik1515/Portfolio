56c107f0962e41f95dbcd5eda5c722c9cbd04241
daivik1515 <daivik.15.saharan@gmail.com>
2025-03-21 21:06:33 -0400

    Undo

--- Portfolio_Daivik/src/app/views/skills/skills.component.ts
+++ Portfolio_Daivik/src/app/views/skills/skills.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css',
-  animations: [
-    trigger('fadeEnlarge', [
-      transition(':leave', [
-        style({ opacity: 1, transform: 'scale(1)' }),
-        animate('1.5s ease-out', style({ opacity: 0, transform: 'scale(1.2)' }))
-      ])
-    ])
-  ]
+  styleUrl: './skills.component.css'
 })
-export class SkillsComponent { }
+export class SkillsComponent {
+
+}
